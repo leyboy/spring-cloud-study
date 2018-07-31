9eaa99b6a788e20093506f8408a0a97f49b24631
leyboy <2036185346@qq.com>
2018-07-31 20:50:11 +0800

    commit eureka-consumer and eureka consumer is use ribbon(client load balanced) v1 (simple)

--- eureka-provider/src/main/java/com/ley/springcloud/provider/controller/HelloController.java
+++ eureka-provider/src/main/java/com/ley/springcloud/provider/controller/HelloController.java
@@ -1,13 +1,26 @@
 package com.ley.springcloud.provider.controller;
 
+import lombok.extern.apachecommons.CommonsLog;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CommonsLog
 public class HelloController {
 
-    @GetMapping(value = "/hello")
-    public String hello(){
-        return "Hello World!";
+    public static final String HELLO_WORLD = "Hello World!";
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping("/hello")
+    public String index() {
+        ServiceInstance serviceInstance = discoveryClient.getLocalServiceInstance();
+        log.info("/hello,host" + serviceInstance.getHost() + ",service_id: " +
+                serviceInstance.getServiceId());
+        return HELLO_WORLD;
     }
 }
